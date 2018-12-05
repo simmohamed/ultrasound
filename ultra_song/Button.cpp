c178246254f1f0e47e87c5832b3bd28c0918d170
simmohamed <44633169+simmohamed@users.noreply.github.com>
2018-12-05 09:51:23 +0200

    Add files via upload

---
+++ ultra_song/Button.cpp
@@ -0,0 +1,26 @@
+/*
+Button.cpp - library for button
+*/
+
+#include "Arduino.h"
+#include "Button.h"
+
+Button::Button(int pin)
+{
+  pinMode(pin, INPUT_PULLUP);
+  _pin=pin;
+  _prevState=digitalRead(pin);
+}
+
+
+
+bool Button::pressed(){
+  bool ret=0;
+  int x=digitalRead(_pin);
+  if(x!=_prevState && x==BUTTONPRESSED){
+    ret=1;
+    delay(BOUNCE);
+  }
+  _prevState=x;
+  return ret;
+} 
---
+++ ultra_song/Button.h
@@ -0,0 +1,25 @@
+/*
+Button.h - library for buttons
+*/
+
+#ifndef Button_h
+#define Button_h
+#include "Arduino.h"
+
+const int BUTTONPRESSED=LOW;
+const int BOUNCE=20;
+
+class Button
+{
+  public:
+    Button(int pin);
+    bool pressed();
+    void downButton();
+    void enterButton();
+    void upButton();
+  private:
+    int _pin;
+    int _prevState;
+};
+
+#endif
